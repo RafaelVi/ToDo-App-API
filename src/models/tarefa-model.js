d2bffaf302903c6ada222e81656e324119279982
RafaelVi <rafaelalmeida383@gmail.com>
2021-04-13 21:25:20 -0300

    Models completely changed, now importing Class objects and keeping old models for future implementations

--- src/models/tarefa-model.js
+++ src/models/tarefa-model.js
@@ -1,38 +1,48 @@
-const conex = require('../infra/conexao');
+// const conex = require('../infra/conexao');
+// class Tarefa{
+//     create(tarefa,res){
+//         // let data = new Date();
+//         // tarefa.date = data.toISOString().slice(0,10);
+//         const sql = 'INSERT INTO TAREFAS(TITULO,DESCRICAO,STATUS,DATACRIACAO,ID_USUARIO) SET (?,?,?,?,?)';
+
+//         conex.run(sql,[tarefa.TITULO,
+//             tarefa.DESCRICAO,
+//             tarefa.STATUS,
+//             tarefa.DATACRIACAO,
+//             tarefa.ID_USUARIO],(err,respo) =>err? res.status(400).json(err): res.status(201).json(tarefa));
+//     }
+//     read(res){
+//         const sql = 'SELECT * FROM TAREFAS';
+//         conex.all(sql,(erro,results) => {if(erro){throw new Error(erro)}else{res.send(results)}});
+//     }
+//     buscaPorID(res,id){
+//         const sql = `SELECT * FROM TAREFAS where id=${id}`;
+
+//         conex.all(sql,(erro,results) => {if(erro){throw new Error(erro)}else{res.send(results[0])}});
+//     }
+//     update(id,tarefas,res){
+//         const sql = `UPDATE TAREFAS SET TITULO=?,DESCRICAO=?,STATUS=?,DATACRIACAO=?,ID_USUARIO=?, where id= ?`;
+
+//         conex.run(sql,[tarefas.TITULO,
+//             tarefas.DESCRICAO,
+//             tarefas.STATUS,
+//             tarefas.DATACRIACAO,
+//             tarefas.ID_USUARIO,id],(erro) => erro? res.status(400).json(erro):res.json({...tarefas,id}));
+//     }
+//     delete(id,res){
+//         const sql = `DELETE FROM TAREFAS where id=${id}`;
+
+//         conex.run(sql,(erro,results) => erro? res.status(400).json(erro):res.json({id}));
+
+//     }
+// }
 class Tarefa{
-    // create(tarefa,res){
-    //     let data = new Date();
-    //     tarefa.date = data.toISOString().slice(0,10);
-    //     const sql = 'INSERT INTO tarefas SET ?';
-
-    //     conex.query(sql,tarefa,(err,respo) =>err? res.status(400).json(err): res.status(201).json(tarefa));
-    // }
-    read(res){
-        const sql = 'SELECT * FROM TAREFAS';
-        conex.all(sql,(erro,results) => {if(erro){throw new Error(erro)}else{res.send(results)}});
+    constructor(body){
+        this.TITULO = body.TITULO;
+        this.DESCRICAO = body.DESCRICAO;
+        this.STATUS = body.STATUS;
+        this.DATACRIACAO = body.DATACRIACAO;
+        this.ID_USUARIO = body.ID_USUARIO;
     }
-    // buscaPorID(res,id){
-    //     const sql = `SELECT * FROM tarefas where id=${id}`;
-
-    //     conex.query(sql,(erro,results) => {
-    //         if(erro) {res.status(400).json(erro)}
-    //         else{
-    //             let formatResults = results[0];
-    //             formatResults.date = formatResults.date.toISOString().slice(0,10);
-    //             res.send(formatResults);
-    //         }});
-    // }
-    // update(id,valores,res){
-    //     const sql = `UPDATE tarefas SET ? where id= ?`;
-
-    //     conex.query(sql,[valores,id],(erro,results) => erro? res.status(400).json(erro):res.json({...valores,id}));
-    // }
-    // delete(id,res){
-    //     const sql = `DELETE FROM tarefas where id=${id}`;
-
-    //     conex.query(sql,(erro,results) => erro? res.status(400).json(erro):res.json({id}));
-
-    // }
 }
-
-module.exports = new Tarefa;
+module.exports = Tarefa;
--- src/models/usuario-model.js
+++ src/models/usuario-model.js
@@ -1,31 +1,36 @@
-const conex = require('../infra/conexao');
+// const conex = require('../infra/conexao');
+// class Usuario{
+//     create(usuario,res){
+//         const sql = 'INSERT INTO USUARIOS(NOME,EMAIL,SENHA) VALUES(?,?,?)';
+//         conex.run(sql,[usuario.NOME,usuario.EMAIL,usuario.SENHA],(erro) => {if(erro){console.log(erro);}else{res.status(201).send('Usuário Criado')}});
+//     }
+//     read(res){
+//         const sql = 'SELECT * FROM USUARIOS';
+//         conex.all(sql,(erro,results) => {if(erro){throw new Error(erro)}else{res.send(results)}});
+//     }
+//     buscaPorID(res,id){
+//         const sql = `SELECT * FROM USUARIOS where id=${id}`;
+//         conex.all(sql,(erro,results) => {if(erro){throw new Error(erro)}else{res.send(results[0])}});
+
+//     }
+//     update(id,valores,res){
+//         const sql = `UPDATE USUARIOS SET NOME = ?, EMAIL = ?, SENHA = ? where id= ?`;
+//         conex.run(sql,[valores.NOME,valores.EMAIL,valores.SENHA,id],(erro,results) => erro? console.log(erro):res.json({...valores,id}));
+//     }
+//     delete(id,res){
+//         const sql = `DELETE FROM USUARIOS where id=${id}`;
+//         conex.run(sql,(erro,results) => erro? res.status(400).json(erro):res.json({id}));
+
+//     }
+// }
+
 class Usuario{
-    // create(usuario,res){
-    //     const sql = 'INSERT INTO usuario SET ?';
-
-    //     conex.query(sql,usuario,(err,results) =>err? res.status(400).json(err): res.status(201).json(usuario));
-    // }
-    read(res){
-        const sql = 'SELECT * FROM USUARIOS';
-        conex.all(sql,(erro,results) => {if(erro){throw new Error(erro)}else{res.send(results)}});
+    constructor(body){
+        this.NOME = body.NOME;
+        this.EMAIL = body.EMAIL;
+        this.SENHA = body.SENHA;
+    
     }
-    // buscaPorID(res,id){
-    //     const sql = `SELECT * FROM usuario where id=${id}`;
-
-    //     conex.query(sql,(erro,results) => erro? res.status(400).json(erro):res.json(results[0]));
-
-    // }
-    // update(id,valores,res){
-    //     const sql = `UPDATE usuario SET ? where id= ?`;
-
-    //     conex.query(sql,[valores,id],(erro,results) => erro? res.status(400).json(erro):res.json({...valores,id}));
-    // }
-    // delete(id,res){
-    //     const sql = `DELETE FROM usuario where id=${id}`;
-
-    //     conex.query(sql,(erro,results) => erro? res.status(400).json(erro):res.json({id}));
-
-    // }
 }
 
-module.exports = new Usuario;
+module.exports = Usuario;
