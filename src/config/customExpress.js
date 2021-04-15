9106e9cada9206c84e441e9f56cecf8c7f7a3dd1
RafaelVi <rafaelalmeida383@gmail.com>
2021-04-14 21:13:01 -0300

    Added midlleware cors

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routeUser = require('../controllers/usuario-controller');
 const routeTask = require('../controllers/tarefa-controller');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 
@@ -9,6 +10,7 @@ module.exports = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
     routeUser(app);
     
     routeTask(app);
