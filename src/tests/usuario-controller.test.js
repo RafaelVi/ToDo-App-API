5a2d4c802d1b6b4d06de5da3c2e81a5424ccbd5f
RafaelVi <rafaelalmeida383@gmail.com>
2021-04-20 18:33:55 -0300

    Search user test added

--- src/tests/usuario-controller.test.js
+++ src/tests/usuario-controller.test.js
@@ -3,8 +3,15 @@ const configExpress = require('../config/customExpress');
 const app = configExpress();
 
 request(app)
-.get('/task')
+.get('/user')
 .expect(200)
 .end(function(err, res) {
-  if (err) throw err; else console.log(res.status);
+  if (err) throw err; else console.log(res.body);
+});
+
+request(app)
+.get('/user/1')
+.expect(200)
+.end(function(err, res) {
+  if (err) throw err; else console.log(res.body);
 });
